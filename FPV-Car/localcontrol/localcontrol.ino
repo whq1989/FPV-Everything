2fd1be4ad73532c8bfd69ccaf6bf8c3e27199091
small <winner304481619@163.com>
2022-02-13 20:57:02 +0800

    整理电机程序

--- FPV-Car/localcontrol/localcontrol.ino
+++ FPV-Car/localcontrol/localcontrol.ino
@@ -7,60 +7,36 @@ int AIN2 = 8; //Direction  方向
 
 void setup(){
 pinMode(STBY, OUTPUT);
-
 pinMode(PWMA, OUTPUT);
 pinMode(AIN1, OUTPUT);
 pinMode(AIN2, OUTPUT);
-
-//pinMode(PWMB, OUTPUT);
-//pinMode(BIN1, OUTPUT);
-//pinMode(BIN2, OUTPUT);
 }
 
 void loop(){
-move(1,90, 1); //motor 1, full speed, left
-//move(2, 255, 1); //motor 2, full speed, left
-
-delay(1000); //go for 1 second
-stop(); //stop
-delay(1250); //hold for 250ms until move again
-
-move(1, 90, 0); //motor 1, half speed, right
-//move(2, 128, 0); //motor 2, half speed, right
-
-delay(1000);
-stop();
-delay(1250);
+  moveMotor(90, 1);
+  stopMotor();
+  moveMotor(90, 0);
+  delay(1000);
 }
 
-void move(int motor, int speed, int direction){
-//Move specific motor at speed and direction
-//motor: 0 for B 1 for A 选择电机
-//speed: 0 is off, and 255 is full speed 选择速度
-//direction: 0 clockwise, 1 counter-clockwise 选择旋转方向： 0表示顺时针，1表示逆时针
 
-digitalWrite(STBY, HIGH); //disable standby取消空置状态
+//------------moveMotor控制电机转速和方向
+void moveMotor(int speed, int direction){
+  //speed: 0 is off, and 255 is full speed 选择速度 75-200
+  //direction: 0 clockwise, 1 counter-clockwise 选择旋转方向： 0表示顺时针，1表示逆时针
+  digitalWrite(STBY, HIGH); //disable standby取消空置状态
+  boolean inPin1 = LOW;
+  boolean inPin2 = HIGH;//电机默认的转动方向
 
-boolean inPin1 = LOW;
-boolean inPin2 = HIGH;
-
-if(direction == 1){
-inPin1 = HIGH;
-inPin2 = LOW;
+  if(direction == 1){//方向置为1时，改变转向
+    inPin1 = HIGH;
+    inPin2 = LOW;
+  }
+  digitalWrite(AIN1, inPin1);//设置方向
+  digitalWrite(AIN2, inPin2);
+  analogWrite(PWMA, speed);//设置速度
 }
-
-if(motor == 1){
-digitalWrite(AIN1, inPin1);
-digitalWrite(AIN2, inPin2);
-analogWrite(PWMA, speed);
-}else{
-//digitalWrite(BIN1, inPin1);
-//digitalWrite(BIN2, inPin2);
-//analogWrite(PWMB, speed);
-}
-}
-
-void stop(){
-//enable standby
-digitalWrite(STBY, LOW);
+//------------stopMotor控制电机转速和方向
+void stopMotor(){//电机待机
+  digitalWrite(STBY, LOW);
 }
